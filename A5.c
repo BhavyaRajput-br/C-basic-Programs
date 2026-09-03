fc60d8e8296f5620df807e37e18ba5b2d4f7fdab
Bhavya Rajput <rajputbhavya100@gmail.com>
2026-09-03 10:46:07 +0530

    Add files via upload

---
+++ A5.c
@@ -0,0 +1,22 @@
+greater between two numbers 
+#include <stdio.h>
+void main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+    {
+        printf("%d is greater", a);
+    }
+    else if (b > a)
+    {
+        printf("%d is greater", b);
+    }
+    else
+    {
+        printf("Both numbers are equal");
+    }
+}
